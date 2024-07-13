47290510bec62b715558c96df8b467db0c64576a
jayanthkrishna <jayanth.krishna.g@gmail.com>
2024-07-13 21:02:41 +0530

    EKS and Jenkins Tf statefile path change

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "jayanth-terraform-projects-tfstatefile-bucket" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "Tetris/EKS/terraform.tfstate"
     region = "ap-south-1"
   }
 }
